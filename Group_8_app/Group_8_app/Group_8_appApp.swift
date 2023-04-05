cb48d5b4b03b9dbd55cac5fc8d5a1e18c6c41eeb
kyrollosceriacous <kyrolosceriacous@gmail.com>
2023-04-05 16:24:14 +0200

    Initial push

---
+++ Group_8_app/Group_8_app/Group_8_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group_8_appApp.swift
+//  Group_8_app
+//
+//  Created by Kyrollos Ceriacous on 2023-04-05.
+//
+
+import SwiftUI
+
+@main
+struct Group_8_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
