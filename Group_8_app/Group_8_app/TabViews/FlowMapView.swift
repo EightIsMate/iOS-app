61d8c9f18858f57e0e364c885cfa3954d19f79c9
niffitek <niklas.scheffler@epitech.eu>
2023-04-14 11:46:05 +0200

    simple test API call to chuck Norris joke API

--- Group_8_app/Group_8_app/TabViews/FlowMapView.swift
+++ Group_8_app/Group_8_app/TabViews/FlowMapView.swift
@@ -7,15 +7,50 @@
 
 import SwiftUI
 
+struct ResponseData: Decodable {
+    let value: String
+}
+
+func APICall(completion: @escaping (String?) -> Void) {
+    let url = URL(string: "https://api.chucknorris.io/jokes/random")!
+    let session = URLSession.shared
+    let task = session.dataTask(with: url) { data, response, error in
+        guard error == nil, let data = data else {
+            print("API Error")
+            completion("API Error")
+            return
+        }
+    
+    let decoder = JSONDecoder()
+        do {
+            let response = try decoder.decode(ResponseData.self, from: data)
+            completion(response.value)
+        } catch {
+            print("Decode Error")
+            completion("Decode Error")
+        }
+    }
+    
+    task.resume()
+}
+
 struct FlowMapView: View {
+    
+    @State private var text = "Loading..."
+    
     var body: some View {
-        ZStack {
-            Circle()
-                .frame(width: 200, height: 200)
-                .foregroundColor(.red)
-            Text("\("Flow map")")
-                .foregroundColor(.white)
-                .font(.system(size: 20, weight: .bold))
+        VStack {
+            Text(text)
+            ZStack {
+                Circle()
+                    .frame(width: 200, height: 200)
+                    .foregroundColor(.red)
+                Text("\("Flow map")")
+                    .foregroundColor(.white)
+                    .font(.system(size: 20, weight: .bold))
+            }
+        }.onAppear {
+            APICall { text in self.text = text ?? "Error"}
         }
     }
 }
