03b3c5b205a1bcb8a01bb0f158421fcaefe85bc8
kyrollosceriacous <kyrolosceriacous@gmail.com>
2023-04-19 17:22:28 +0200

    Left and right arrows change colors when holding them down and also continously send out a value in the meantime

--- Group_8_app/Group_8_app/ControllerButtons/LeftRightArrowsView.swift
+++ Group_8_app/Group_8_app/ControllerButtons/LeftRightArrowsView.swift
@@ -8,28 +8,62 @@
 import SwiftUI
 
 struct LeftRightArrowsView: View {
+    @State var leftArrowBackgroundColor = Color(hex: 0x273a60)
+    @State var rightArrowBackgroundColor = Color(hex: 0x273a60)
+    @GestureState private var isPressedLeft = false
+    @GestureState private var isPressedRight = false
+
+    @State private var leftTimer: Timer?
+    @State private var rightTimer: Timer?
+
     var body: some View {
         VStack {
             HStack {
-                Image(systemName: "arrow.left")
-                    .resizable()
-                    .frame(width: 75, height: 75)
-                    .foregroundColor(.white)
-                    .background(Color(hex: 0x273a60))
-                    .cornerRadius(20)
-                    .onTapGesture {
-                        print("left arrow pressed")
+                // Left arrow button
+                Button(action: {}) {
+                    Image(systemName: "arrow.left")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                        .foregroundColor(.white)
+                        .background(isPressedLeft ? Color.green : leftArrowBackgroundColor)
+                        .cornerRadius(20)
+                }
+                .simultaneousGesture(LongPressGesture(minimumDuration: .infinity).updating($isPressedLeft) { (value, state, transaction) in
+                    state = true
+                })
+                .onChange(of: isPressedLeft) { isPressed in
+                    if isPressed {
+                        leftTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
+                            print("left arrow is being held down")
+                        }
+                    } else {
+                        leftTimer?.invalidate()
+                        leftTimer = nil
                     }
-                
-                Image(systemName: "arrow.right")
-                    .resizable()
-                    .frame(width: 75, height: 75)
-                    .foregroundColor(.white)
-                    .background(Color(hex: 0x273a60))
-                    .cornerRadius(20)
-                    .onTapGesture {
-                        print("right arrow pressed")
+                }
+
+                // Right arrow button
+                Button(action: {}) {
+                    Image(systemName: "arrow.right")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                        .foregroundColor(.white)
+                        .background(isPressedRight ? Color.green : rightArrowBackgroundColor)
+                        .cornerRadius(20)
+                }
+                .simultaneousGesture(LongPressGesture(minimumDuration: .infinity).updating($isPressedRight) { (value, state, transaction) in
+                    state = true
+                })
+                .onChange(of: isPressedRight) { isPressed in
+                    if isPressed {
+                        rightTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
+                            print("right arrow is being held down")
+                        }
+                    } else {
+                        rightTimer?.invalidate()
+                        rightTimer = nil
                     }
+                }
             }
         }
         .padding(35)
@@ -42,3 +76,4 @@ struct LeftRightArrowsView_Previews: PreviewProvider {
         LeftRightArrowsView()
     }
 }
+
