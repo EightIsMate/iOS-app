d2447e2f41f5411c9b9159a9404dbf1b9df25e1a
niffitek <niklas.scheffler@epitech.eu>
2023-04-28 17:56:38 +0200

    add obstacle route

--- Group_8_app/Group_8_app/TabViews/FlowMapView.swift
+++ Group_8_app/Group_8_app/TabViews/FlowMapView.swift
@@ -12,11 +12,13 @@ struct ResponseData: Decodable {
     let position_vertical: String
     let timestamp: String
     let id: UUID
+    let position_type_id: UUID
+    let name: String
 }
 
 
-func APICall(completion: @escaping ([ResponseData]) -> Void) {
-    let url = URL(string: "https://ims8.herokuapp.com/positions")!
+func GetPositions(completion: @escaping ([ResponseData]) -> Void) {
+    let url = URL(string: "https://ims8.herokuapp.com/positions/mover")!
     let session = URLSession.shared
     let task = session.dataTask(with: url) { data, response, error in
         guard error == nil, let data = data else {
@@ -28,7 +30,6 @@ func APICall(completion: @escaping ([ResponseData]) -> Void) {
     let decoder = JSONDecoder()
         do {
             let response = try decoder.decode(Array<ResponseData>.self, from: data)
-            print(response)
             completion(response)
         } catch let error {
             print(error)
@@ -39,10 +40,34 @@ func APICall(completion: @escaping ([ResponseData]) -> Void) {
     task.resume()
 }
 
+func GetObstacles(completion: @escaping ([ResponseData]) -> Void) {
+    let url = URL(string: "https://ims8.herokuapp.com/positions/obstacle")!
+    let session = URLSession.shared
+    let task = session.dataTask(with: url) { data, response, error in
+        guard error == nil, let data = data else {
+            print("API Error")
+            completion([])
+            return
+        }
+    
+    let decoder = JSONDecoder()
+        do {
+            let response = try decoder.decode(Array<ResponseData>.self, from: data)
+            completion(response)
+        } catch let error {
+            print(error)
+            completion([])
+        }
+    }
+    
+    task.resume()
+}
+
+
 struct FlowMapView: View {
     
-    @State private var points: [(Double, Double)] = [(0.0, 0.0)]
-    
+    @State private var mower: [(Double, Double)] = [(-1.0, -1.0)]
+    @State private var obstacles: [(Double, Double)] = [(-1.0, -1.0)]
     var body: some View {
         GeometryReader { geometry in
             ZStack {
@@ -59,32 +84,48 @@ struct FlowMapView: View {
                 }
                 .stroke()
                 
+                Path { path in
+                    for (_, point) in obstacles.enumerated() {
+                        let point = CGPoint(x: point.0 + 40, y: geometry.size.height - point.1 - 20)
+                        path.addRect(CGRect(x: point.x - 5, y: point.y - 5, width: 10, height: 10))}
+                }
+                .fill(Color.red)
+                
                 // Add points
                 Path { path in
-                    for (i, point) in points.enumerated() {
+                    for (i, point) in mower.enumerated() {
                         let point = CGPoint(x: point.0 + 40, y: geometry.size.height - point.1 - 20)
-                        if i != points.count - 1 {
+                        if i != mower.count - 1 {
                             path.addEllipse(in: CGRect(x: point.x - 5, y: point.y - 5, width: 10, height: 10))}
                     }
                 }
                 .fill(Color.gray)
                 
                 Path { path in
-                    let pos = points[points.count - 1]
+                    let pos = mower[mower.count - 1]
                     let point = CGPoint(x: pos.0 + 40, y: geometry.size.height - pos.1 - 20)
                             path.addEllipse(in: CGRect(x: point.x - 5, y: point.y - 5, width: 10, height: 10))
                 }
                 .fill(Color.blue)
+                
+             
             }
             .frame(width: geometry.size.width, height: geometry.size.height)
             .onAppear {
-                APICall { response in
+                GetPositions { response in
                     // Convert ResponseData structs to (Double, Double) tuples
                     var tuples = response.map { (Double($0.position_horizontal) ?? 0, Double($0.position_vertical) ?? 0) }
                     // Update the @State variable with the tuples
                     tuples = tuples.map { ($0 * 5, $1 * 5) }
-                    points = tuples
-                    
+                    mower = tuples
+                };
+                GetObstacles { response in
+                    // Convert ResponseData structs to (Double, Double) tuples
+                    var tuples = response.map { (Double($0.position_horizontal) ?? 0, Double($0.position_vertical) ?? 0) }
+                    // Update the @State variable with the tuples
+                    tuples = tuples.map { ($0 * 5, $1 * 5) }
+                    obstacles = tuples
+                    print(obstacles)
                 }
             }
         }.padding(10)
